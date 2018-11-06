ccad4ccc06570a5aab47a9df7694b72e97f4a9b5
Reggyboy <42719033+Reggyboy@users.noreply.github.com>
2018-11-06 17:11:07 +0100

    Update user.js

--- server/user.js
+++ server/user.js
@@ -14,13 +14,14 @@ router.post('/api/user', async function(req, res, next) {
 
   if(queryUser) {
     res.status(403).json({}).end();
-  }
+  } else {
 
-  let query = `INSERT INTO "public"."users" ("id", "username", "email", "password", "role")
-  VALUES (DEFAULT, '${userName}', '${userEmail}', '${userPass}', '${userRole}')`;
+  let query = `INSERT INTO "public"."users" ("email", "username", "hash", "role", "id") 
+VALUES ('${userEmail}', '${userName}', '${userPass}, '${userRole}', DEFAULT)`;
 
   let status = await db.insert(query) ? 200 : 500;
   res.status(status).json({}).end();
+  }
 });
 
 // Logging in
@@ -29,11 +30,13 @@ router.post('/api/users/auth', async function(req, res, next) {
   let userPass = req.body.pass;
 
   let query = `SELECT * FROM public.users t
-  WHERE username = '${userName}' and password = '${userPass}'`;
+  WHERE username = '${userName}' and hash = '${userPass}'`;
 
   let user = db.select(query);
   if(user) {
     res.status(200).json(user).end();
+    //---store datahere
+    console.log("loged in");
   } else {
     res.status(401).json({}).end();
   }
